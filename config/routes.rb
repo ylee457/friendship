02e097d41666bfab14479f2e63350d618f260baa
Yong Lee <ylee457@gmail.com>
2017-04-01 00:05:28 -0700

    no message

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
   root 'homes#index'
 
   resources :users
+  get 'profile', to: 'users#show'
 
+  
   # Devise routes
-  devise_for :users
+  # specify path param to avoid conflict with users path
+  devise_for :users, controllers: { registrations: "registrations"}, path: 'accounts'
+
+  
+
 
   
 end
